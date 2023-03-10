649b0278a96ca9589991a1f8a9512dc29da1ff79
Valerian <chinecheremvalarian@gmail.com>
2023-03-10 13:39:30 +0100

    modified works section

--- my_portfolio/static/js/main.js
+++ my_portfolio/static/js/main.js
@@ -90,6 +90,101 @@ var TxtRotate = function(el, toRotate, period) {
     document.body.appendChild(css);
   };
 
+  // let myCarousel = document.querySelectorAll('#featureContainer .carousel .carousel-item');
+  // myCarousel.forEach((el) => {
+  //   const minPerSlide = 4
+  //   let next = el.nextElementSibling
+  //   for (var i=1; i<minPerSlide; i++) {
+  //     if (!next) {
+  //       // wrap carousel by using first child
+  //       next = myCarousel[0]
+  //     }
+  //     let cloneChild = next.cloneNode(true)
+  //     el.appendChild(cloneChild.children[0])
+  //     next = next.nextElementSibling
+  //   }
+  // })
+
+  // window.dataLayer = window.dataLayer || [];
+  // function gtag(){dataLayer.push(arguments);}
+  // gtag('js', new Date());
+  // gtag('config', 'G-LLWL5N9CSM');
+
+  // carousel
+//   const carousel = document.querySelector(".carousel"),
+// firstImg = carousel.querySelectorAll("img")[0],
+// arrowIcons = document.querySelectorAll(".row5 i");
+
+// let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+
+// const showHideIcons = () => {
+//     // showing and hiding prev/next icon according to carousel scroll left value
+//     let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+//     arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+//     arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+// }
+
+// arrowIcons.forEach(icon => {
+//     icon.addEventListener("click", () => {
+//         let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
+//         // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+//         carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+//         setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
+//     });
+// });
+
+// const autoSlide = () => {
+//     // if there is no image left to scroll then return from here
+//     if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
+
+//     positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
+//     let firstImgWidth = firstImg.clientWidth + 14;
+//     // getting difference value that needs to add or reduce from carousel left to take middle img center
+//     let valDifference = firstImgWidth - positionDiff;
+
+//     if(carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
+//         return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+//     }
+//     // if user is scrolling to the left
+//     carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+// }
+
+// const dragStart = (e) => {
+//     // updatating global variables value on mouse down event
+//     isDragStart = true;
+//     prevPageX = e.pageX || e.touches[0].pageX;
+//     prevScrollLeft = carousel.scrollLeft;
+// }
+
+// const dragging = (e) => {
+//     // scrolling images/carousel to left according to mouse pointer
+//     if(!isDragStart) return;
+//     e.preventDefault();
+//     isDragging = true;
+//     carousel.classList.add("dragging");
+//     positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+//     carousel.scrollLeft = prevScrollLeft - positionDiff;
+//     showHideIcons();
+// }
+
+// const dragStop = () => {
+//     isDragStart = false;
+//     carousel.classList.remove("dragging");
+
+//     if(!isDragging) return;
+//     isDragging = false;
+//     autoSlide();
+// }
+
+// carousel.addEventListener("mousedown", dragStart);
+// carousel.addEventListener("touchstart", dragStart);
+
+// document.addEventListener("mousemove", dragging);
+// carousel.addEventListener("touchmove", dragging);
+
+// document.addEventListener("mouseup", dragStop);
+// carousel.addEventListener("touchend", dragStop);
+
 
   // gif_image_icons
   // function gifImg(e){
