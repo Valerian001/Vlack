84342cebcfec78f060689e23cb4054bfa0ea4d2d
Valerian <chinecheremvalarian@gmail.com>
2023-03-20 04:49:54 +0100

    hero & about section responsive

--- my_portfolio/my_portfolio/settings.py
+++ my_portfolio/my_portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure--x27ie4xqus7((p$q#00%=fjdl%$i%sb_werrj206=tq6_bp5l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.43.164']
 
 
 # Application definition
