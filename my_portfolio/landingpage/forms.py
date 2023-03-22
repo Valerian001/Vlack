ed6d6b33c25d734c497e89e1f689125d0569f6c7
Valerian <chinecheremvalarian@gmail.com>
2023-03-22 17:15:59 +0100

    finished form

--- my_portfolio/landingpage/forms.py
+++ my_portfolio/landingpage/forms.py
@@ -1,20 +1,7 @@
 from django.forms import ModelForm
 from .models import FormModel
 
-# class ContactForm(forms.Form):
-#     your_Name = forms.CharField(max_length=100)
-#     your_Email = forms.EmailField()
-#     your_Phone = forms.CharField(max_length=11)
-#     your_Company = forms.CharField(max_length=100)
-#     Message = forms.CharField(widget=forms.Textarea)
-
 class ContactForm(ModelForm):
-    # your_Name = forms.CharField(max_length=100)
-    # your_Email = forms.EmailField()
-    # your_Phone = forms.CharField(max_length=11)
-    # your_Company = forms.CharField(max_length=100)
-    # Message = forms.CharField(widget=forms.Textarea)
-    # specify the name of model to use
     class Meta:
         model = FormModel
         fields = "__all__"
